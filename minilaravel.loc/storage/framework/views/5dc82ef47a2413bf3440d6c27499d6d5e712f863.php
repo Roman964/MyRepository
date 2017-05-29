4ccf9b8787516d50d28363fb7fd9d22e22cf007f
Roman <sprite6121716@gmail.com>
2017-05-30 00:01:58 +0300

    Changes_2

---
+++ minilaravel.loc/storage/framework/views/5dc82ef47a2413bf3440d6c27499d6d5e712f863.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+<title>Contacts</title>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+<link rel="stylesheet" href="/tutorials/style.css" type="text/css" media="screen">
+<link href="<?php echo e(asset('css/bootstrap.min.css')); ?>" rel = "stylesheet">
+	
+
+	
+
+	</head>
+
+	<body>
+
+<p>
+Wanna contact me? Its not a problem: +375296121716 (velcome);
+sprite6121716@gmail.com
+
+<p>
+<a href="http://minilaravel.loc/">Back to main page</a>
+<a href=""><img src=""></a>
+	</body>
+</html>
